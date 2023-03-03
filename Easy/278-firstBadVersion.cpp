658a55074bfc2ac04180060220b8a8ddc002cacb
trehanpraharsh <trehanpraharsh@gmail.com>
2023-03-03 17:45:41 +0530

    3rd March's Question Solved - Easy Question

---
+++ Easy/278-firstBadVersion.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+#include <string.h>
+#include <vector>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+
+bool isBadVersion(int n){
+    if (n == 4) return true;
+    return false;
+}
+
+int firstBadVersion(int n) {
+    int left = 1;
+    int right = n;
+    while (left < right) {
+        int mid = left + (right - left) / 2;
+        if (isBadVersion(mid)) {
+            right = mid;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return left;
+}
+
+int main(){
+    int n = 5;
+    cout<<firstBadVersion(n)<<endl;
+    return 0;
+}
